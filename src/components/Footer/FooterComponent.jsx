13833dc97f0845680647cec130dd46c2d93e56cc
Julio Ucharima <julio_ucharima@usmp.pe>
2023-09-07 12:41:38 -0500

    Creando componente Footer

---
+++ src/components/Footer/FooterComponent.jsx
@@ -0,0 +1,27 @@
+import Logo from "/Logo.svg";
+import "./Footer.css";
+
+export const FooterComponent = () => {
+  return (
+    <footer
+      className="footer"
+      style={{ backgroundImage: "url(/image/footer.png)" }}
+    >
+      <div className="redes">
+        <a href="#">
+          <img src="/image/facebook.png" alt="Facebook" />
+        </a>
+        <a href="#">
+          <img src="/image/twitter.png" alt="Twitter" />
+        </a>
+        <a href="#">
+          <img src="/image/instagram.png" alt="Instagram" />
+        </a>
+      </div>
+      <img src={Logo} alt="org" />
+      <p>
+        Desarrollado por <a href="">AngelOU20</a>
+      </p>
+    </footer>
+  );
+};
