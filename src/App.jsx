769ff2d22a1f753f20926ad8e5ac521337499ecd
Julio Ucharima <julio_ucharima@usmp.pe>
2023-09-07 14:58:09 -0500

    Actualizando App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,53 @@
 import { useState } from "react";
-import { FormComponent, HeaderComponent, MiOrgComponent } from "./components";
+import {
+  EquipoComponent,
+  FooterComponent,
+  FormComponent,
+  HeaderComponent,
+  MiOrgComponent,
+} from "./components";
+import { equiposData, colaboradoresData } from "./data";
 
 function App() {
   const [mostrarForm, setMostrarForm] = useState(true);
+  const [colaboradores, setColaboradores] = useState(colaboradoresData);
 
   const cambiarMostrar = () => {
     setMostrarForm(!mostrarForm);
   };
 
+  const registrarColaborador = (colaborador) => {
+    console.log("Nuevo colaborador", colaborador);
+    //Spread operator
+    setColaboradores([...colaboradores, colaborador]);
+  };
+
+  console.log(colaboradores);
+
   return (
     <>
       <HeaderComponent />
-      {mostrarForm && <FormComponent />}
+
+      {mostrarForm && (
+        <FormComponent
+          equipos={equiposData.map((equipo) => equipo.titulo)}
+          registrarColaborador={registrarColaborador}
+        />
+      )}
+
       <MiOrgComponent cambiarMostrar={cambiarMostrar} />
+
+      {equiposData.map((equipo) => (
+        <EquipoComponent
+          key={equipo.titulo}
+          {...equipo}
+          colaboradores={colaboradores.filter(
+            (colaborador) => colaborador.equipo === equipo.titulo
+          )}
+        />
+      ))}
+
+      <FooterComponent />
     </>
   );
 }
